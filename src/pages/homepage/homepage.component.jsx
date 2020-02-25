62f0a4fad137ec5ce5275091f1ec20a41c768c39
orlandobarrera6 <orlandobarrera6@gmail.com>
2020-02-25 11:16:45 -0500

    broken down app into components and added necessary css featuresto menu item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+
+import Directory from '../../components/directory/directory.componentn';
+
+import './homepage.styles.scss';
+
+const HomePage = () => {
+    
+    return(
+        <div className='homepage'>
+            <Directory />
+        </div>
+    );
+
+}
+
+export default HomePage;
