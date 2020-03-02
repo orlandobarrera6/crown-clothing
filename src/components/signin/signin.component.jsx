71c8a13d4a33cdb6a331173e65686388c8aafdc2
orlandobarrera6 <orlandobarrera6@gmail.com>
2020-03-02 10:35:31 -0500

    creating of custom button component for flexible reusability through out the the entire project

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./signin.styles.scss";
 
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends React.Component {
 	constructor() {
@@ -51,7 +52,7 @@ class SignIn extends React.Component {
 						required
 					/>
 
-					<input type='submit' value='Submit Form' />
+					<CustomButton type='submit'>sign in</CustomButton>
 				</form>
 			</div>
 		);
