1de249bfd87bbd36a2cfa5016ff9220d939bc861
orlandobarrera6 <orlandobarrera6@gmail.com>
2020-03-09 15:08:19 -0400

    added cart utilities to add functionality of adding multiple items of the same kind and group them in the cart, as well as updated the SHOP_DATA

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CustomButton from "../../components/custom-button/custom-button.component";
+
+import { addItem } from "../../redux/cart/cart.actions";
 
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ key, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+	const { name, price, imageUrl } = item;
+
 	return (
 		<div className='collection-item'>
 			<div
@@ -13,10 +20,17 @@ const CollectionItem = ({ key, name, price, imageUrl }) => {
 			/>
 			<div className='collection-footer'>
 				<span className='name'>{name}</span>
-				<span className='prce'>{price}</span>
+				<span className='price'>{price}</span>
 			</div>
+			<CustomButton inverted onClick={() => addItem(item)}>
+				Add to Cart
+			</CustomButton>
 		</div>
 	);
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+	addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,11 +1,19 @@
 import CartActionTypes from "./cart.types";
 
+import { addItemToCart } from "./cart.utils";
+
 const INITIAL_STATE = {
-	hidden: true
+	hidden: true,
+	cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
 	switch (action.type) {
+		case CartActionTypes.ADD_ITEM:
+			return {
+				...state,
+				cartItems: addItemToCart(state.cartItems, action.payload)
+			};
 		case CartActionTypes.TOGGLE_CART_HIDDEN:
 			return {
 				...state,
