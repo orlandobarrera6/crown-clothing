ceb924ee0e4482ea70d7c0ecb77ed07497d07e84
orlandobarrera6 <orlandobarrera6@gmail.com>
2020-04-09 14:43:05 -0400

    setting uo shop page for advanced nested routing

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -12,7 +12,7 @@ const CollectionPreview = ({ title, items }) => {
 				{items
 					// .filter(items => items.id < 5)
 					.filter((item, idx) => idx < 4)
-					.map(item => (
+					.map((item) => (
 						<CollectionItem key={item.id} item={item} />
 					))}
 			</div>
--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -1,32 +1,13 @@
 import React from "react";
 
-import { connect } from "react-redux";
+import CollectionOverview from "../../components/collection-overview/collection-overview.component";
 
-import { createStructuredSelector } from "reselect";
-import { selectShopCollections } from "../../redux/shop/shop.selectors";
-
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-
-const ShopPage = ({ collections }) => {
+const ShopPage = () => {
 	return (
 		<div className='shop-page'>
-			{collections.map(({ id, ...otherCollectionProps }) => (
-				<CollectionPreview key={id} {...otherCollectionProps} />
-			))}
+			<CollectionOverview />
 		</div>
 	);
 };
 
-// This is the easy way of mapping the global state to props
-
-// const mapStateToProps = (state) => ({
-// 	collections: state.shop.collections,
-// });
-
-// However we will use selectors for the collections
-
-const mapStateToProps = createStructuredSelector({
-	collections: selectShopCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
